a44cd4da7d92277448b40ccbbaa8696750931692
Antal Orcsik <aorcsik@hu.ibm.com>
2017-06-08 13:26:51 +0200

    use custom casper theme

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-ncp node_modules/Casper content/themes/casper
+if [ ! -d "content/themes/casper/.git" ]; then
+    ncp node_modules/Casper content/themes/casper
+fi
 
 ncp node_modules/prismjs/prism.js public/prism.js
 ncp node_modules/prismjs/themes/prism.css public/prism.css
